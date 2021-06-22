7230e425869026b88e168f313a902406c19f84d1
unknown <kcdrez@gmail.com>
2021-06-22 10:32:52 -0600

    build

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router';
 import HomePage from '../pages/homepage.vue';
 
 const routes = [
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  // history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
