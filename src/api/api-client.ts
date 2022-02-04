906d26cf5a50b76b0a07cb6d5bd5777d0f140bc2
KC Drez <kcdrez@KCs-MacBook-Pro.local>
2022-02-03 17:21:31 -0700

    base url change from local to deployed

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -6,9 +6,9 @@ import { Unit, UnitBasic } from "../types/unit";
 import { RelicConfigType } from "../types/relic";
 
 class ApiClient {
-  // baseUrl = "https://vkpnob5w55.execute-api.us-east-1.amazonaws.com/dev";
+  baseUrl = "https://vkpnob5w55.execute-api.us-east-1.amazonaws.com/dev";
   // baseUrl = "http://7739-184-96-186-220.ngrok.io";
-  baseUrl = "http://localhost:3000/dev";
+  // baseUrl = "http://localhost:3000/dev";
 
   constructor() {}
 
