53458eddaaf384fc812f6dfba8a9883c067806c3
KC Drez <kcdrez@KCs-MacBook-Pro.local>
2022-02-03 20:46:41 -0700

    added feedback when nothing in general planner

--- src/pages/generalPlanner.vue
+++ src/pages/generalPlanner.vue
@@ -1,9 +1,16 @@
 <template>
   <Loading :state="requestState" message="Loading Planner Data" size="lg">
     <div class="container general-planner-page">
-      <UnitSection />
-      <GearSection />
-      <RelicSection />
+      <div v-if="fullUnitList.length === 0">
+        You have no units in the General Planner. Navigate to the
+        <router-link :to="{ name: 'home' }">home page</router-link> and select a
+        unit to add to the General Planner.
+      </div>
+      <template v-else>
+        <UnitSection />
+        <GearSection />
+        <RelicSection />
+      </template>
     </div>
   </Loading>
 </template>
@@ -22,6 +29,7 @@ export default defineComponent({
   components: { UnitSection, GearSection, RelicSection },
   computed: {
     ...mapGetters(["someLoading"]),
+    ...mapGetters("planner", ["fullUnitList"]),
     requestState(): loadingState {
       return this.someLoading(["planner", "unit", "gear", "relic", "planner"]);
     },
