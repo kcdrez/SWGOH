9a4b0d3af11e9cf4e71a46a22045a3d59f8a5389
KC Drez <kcdrez@KCs-MacBook-Pro.local>
2022-04-12 11:00:09 -0600

    added references

--- src/pages/widgets.vue
+++ src/pages/widgets.vue
@@ -9,6 +9,9 @@ import { defineComponent } from "vue";
 
 import WidgetGrid from "../components/widgets/WidgetGrid.vue";
 
+//https://codesandbox.io/s/0z9kv?file=/src/components/GridComponent.vue
+//https://codepen.io/niklasramo/details/bGWpVae
+
 export default defineComponent({
   name: "WidgetsPage",
   components: { WidgetGrid },
