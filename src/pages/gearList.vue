d56d21c992de21c2af843a33d040e7ba079ab110
KC Drez <kcdrez@KCs-MacBook-Pro.local>
2022-04-12 16:44:20 -0600

    added initial gear list page

---
+++ src/pages/gearList.vue
@@ -0,0 +1,30 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapActions, mapGetters, mapState } from "vuex";
+
+import UnitSection from "../components/generalPlanner/unitSection.vue";
+import GearSection from "../components/generalPlanner/gearSection.vue";
+import RelicSection from "../components/generalPlanner/relicSection.vue";
+import ShardSection from "../components/generalPlanner/shardSection.vue";
+import UnitSearch from "../components/units/unitSearch.vue";
+import { loadingState } from "../types/loading";
+import { Unit } from "../types/unit";
+import { initializeModules } from "../utils";
+
+const dependencyModules = ["planner", "unit", "gear", "relic", "shards"];
+
+export default defineComponent({
+  name: "GearListPage",
+  components: {},
+  data() {},
+  computed: {},
+  methods: {},
+  created() {},
+});
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+</style>
