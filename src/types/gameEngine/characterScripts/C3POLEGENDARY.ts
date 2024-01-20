3aedeceb7c3b4570a5c2272f41c719684ae44579
KC Drez <kcdrez@gmail.com>
2024-01-19 23:18:20 -0700

    fixed limit

---
+++ src/types/gameEngine/characterScripts/C3POLEGENDARY.ts
@@ -0,0 +1,307 @@
+import { v4 as uuid } from "uuid";
+
+import {
+  ActiveAbility,
+  PassiveAbility,
+} from "types/gameEngine/characters/abilities";
+import { Character } from "../characters/index";
+import { iStatsCheck } from "../characters/stats";
+import { iCondition } from "../gameEngine";
+
+class basicskill_C3POLEGENDARY extends ActiveAbility {
+  constructor(character: Character) {
+    super(
+      "basicskill_C3POLEGENDARY",
+      "Baffling Trick",
+      `C-3PO inflicts the unique debuff Confuse for 3 turns (max 3 stacks, can't be evaded or copied). If target is already Confused, duration of their stacks resets to 3 turns. Reduce target's Turn Meter by 6% and 3% more for each stack of Translation on C-3PO. (See Protocol Droid for Translation.)
+      \n\n
+      Confuse - Detrimental effects build based on the cumulative number of stacks:
+      1: Cannot gain buffs
+      2: Cannot counter, assist, or gain bonus Turn Meter (Raid bosses and Galactic Legends: -30% Counter Chance)
+      3: When this character uses their Basic ability, increase their cooldowns by 1, which can't be resisted (Raid bosses and Galactic Legends: -50% Defense, doesn't stack with Defense Down)`,
+      character
+    );
+  }
+
+  public execute(
+    targetCharacter?: Character,
+    stats?: iStatsCheck[],
+    canBeCountered: boolean = true
+  ): void {
+    const { targetList, primaryTarget } = this.findTargets(
+      {
+        filters: [{ allies: false }],
+        targetCount: 1,
+      },
+      targetCharacter
+    );
+
+    super.execute(primaryTarget, stats, canBeCountered, () => {
+      targetList.forEach((target) => {
+        if (!this.checkEvade("physical", target)) {
+          this.dealDamage("physical", target, 1.781, 5, stats, canBeCountered);
+          if (
+            target.checkCondition({
+              debuffs: ["Speed Down"],
+            })
+          ) {
+            target.changeTurnMeter(-30, this);
+          }
+
+          if (
+            target.checkCondition({
+              debuffs: ["Defense Down"],
+            })
+          ) {
+            this._character?.statusEffect.inflictDebuff(
+              [{ name: "Stun", duration: 1, id: uuid() }],
+              target,
+              1,
+              this
+            );
+          }
+
+          this._character?.statusEffect.inflictDebuff(
+            [
+              { name: "Speed Down", duration: 2, id: uuid() },
+              { name: "Defense Down", duration: 2, id: uuid() },
+            ],
+            target,
+            1,
+            this
+          );
+        }
+      });
+    });
+  }
+}
+
+class specialskill_COMMANDERLUKESKYWALKER02 extends ActiveAbility {
+  constructor(character: Character) {
+    super(
+      "specialskill_COMMANDERLUKESKYWALKER02",
+      "Call to Action",
+      `Dispel all debuffs on Luke. Luke gains 100% Turn Meter and recovers 40% Health and Protection. If Luke doesn't have Call to Action, he gains it until the next time this ability is used, which can't be copied, dispelled, or prevented. If Luke already had Call to Action, he removes it.\n\nCall to Action: This character ignores Taunt during their turn and has +50% Accuracy, Critical Chance, and Critical Damage`,
+      character
+    );
+    this.cooldown = 4;
+  }
+
+  public execute(): void {
+    super.execute(null, [], false, () => {
+      this._character?.statusEffect.removeDebuff("all", this._character);
+      this._character?.changeTurnMeter(100, this);
+      this._character?.heal(
+        {
+          healthType: "protection",
+          amountType: "multiplicative",
+          amount: 0.4,
+        },
+        this
+      );
+      this._character?.heal(
+        {
+          healthType: "health",
+          amountType: "multiplicative",
+          amount: 0.4,
+        },
+        this
+      );
+
+      if (
+        this._character?.checkCondition({
+          buffs: ["Call to Action"],
+        })
+      ) {
+        this._character?.statusEffect.removeBuff(
+          "Call to Action",
+          this._character
+        );
+      } else {
+        this._character?.statusEffect.addBuff(
+          [
+            {
+              name: "Call to Action",
+              duration: Infinity,
+              id: uuid(),
+              cantDispel: true,
+              cantResist: true,
+              unique: true,
+            },
+          ],
+          1,
+          this
+        );
+      }
+    });
+  }
+}
+
+class uniqueskill_COMMANDERLUKESKYWALKER02 extends PassiveAbility {
+  constructor(character: Character) {
+    super(
+      "uniqueskill_COMMANDERLUKESKYWALKER02",
+      "It Binds All Things",
+      `Luke has +40% Potency. Whenever Luke Resists a detrimental effect he recovers 5% Health and 5% Protection. Whenever Luke inflicts a debuff he gains 10% Turn Meter and other allies gain half that amount.`,
+      character
+    );
+  }
+
+  public override activate(): void {
+    this._character?.stats.tempStats.push({
+      statToModify: "potency",
+      amount: 0.4,
+      modifiedType: "additive",
+    });
+
+    this._character?.events.push(
+      {
+        characterSourceId: this._character.uniqueId,
+        eventType: "resisted",
+        callback: () => {
+          this._character?.heal(
+            {
+              amount: 0.05,
+              healthType: "protection",
+              amountType: "multiplicative",
+            },
+            this
+          );
+          this._character?.heal(
+            {
+              amount: 0.05,
+              healthType: "health",
+              amountType: "multiplicative",
+            },
+            this
+          );
+        },
+      },
+      {
+        characterSourceId: this._character.uniqueId,
+        eventType: "inflicted",
+        callback: () => {
+          this._character?.changeTurnMeter(10, this);
+          this._character?.teammates.forEach((target) => {
+            if (!target.isSelf(this._character)) {
+              target.changeTurnMeter(5, this);
+            }
+          });
+        },
+      }
+    );
+  }
+}
+
+class uniqueskill_COMMANDERLUKESKYWALKER01 extends PassiveAbility {
+  constructor(character: Character) {
+    super(
+      "uniqueskill_COMMANDERLUKESKYWALKER01",
+      "Learn Control",
+      `While Luke doesn't have Call to Action, he has +50% Counter Chance, +50% Critical Avoidance, +50% Defense, +100% Tenacity, and gains 10% Turn Meter whenever another Rebel ally takes damage.`,
+      character
+    );
+  }
+
+  public override activate(): void {
+    const doesntHaveCallToAction: iCondition = {
+      buffs: ["Call to Action"],
+      inverted: true,
+    };
+
+    this._character?.stats.tempStats.push({
+      statToModify: "counterChance",
+      amount: 0.5,
+      modifiedType: "additive",
+      condition: doesntHaveCallToAction,
+      characterSourceId: this._character.uniqueId,
+    });
+
+    this._character?.stats.tempStats.push(
+      {
+        statToModify: "physicalCritAvoid",
+        amount: 0.5,
+        modifiedType: "additive",
+        condition: doesntHaveCallToAction,
+        characterSourceId: this._character.uniqueId,
+      },
+      {
+        statToModify: "specialCritAvoid",
+        amount: 0.5,
+        modifiedType: "additive",
+        condition: doesntHaveCallToAction,
+        characterSourceId: this._character.uniqueId,
+      }
+    );
+
+    this._character?.stats.tempStats.push(
+      {
+        statToModify: "physicalArmor",
+        amount: 0.5,
+        modifiedType: "multiplicative",
+        condition: doesntHaveCallToAction,
+        characterSourceId: this._character.uniqueId,
+      },
+      {
+        statToModify: "specialArmor",
+        amount: 0.5,
+        modifiedType: "multiplicative",
+        condition: doesntHaveCallToAction,
+        characterSourceId: this._character.uniqueId,
+      }
+    );
+
+    this._character?.stats.tempStats.push({
+      statToModify: "tenacity",
+      amount: 1,
+      modifiedType: "additive",
+      condition: doesntHaveCallToAction,
+      characterSourceId: this._character.uniqueId,
+    });
+
+    const { targetList } = this.findTargets({
+      filters: [{ allies: true }, { tags: ["Rebel & !Self"] }],
+    });
+
+    targetList.forEach((target) => {
+      target.events.push({
+        characterSourceId: this._character?.uniqueId,
+        eventType: "receiveDamage",
+        callback: () => {
+          this._character?.changeTurnMeter(10, this);
+        },
+      });
+    });
+  }
+}
+
+const basicAbility = new Map([
+  ["basicskill_C3POLEGENDARY", basicskill_C3POLEGENDARY],
+]);
+
+const specialAbilities = new Map([
+  [
+    "specialskill_COMMANDERLUKESKYWALKER02",
+    specialskill_COMMANDERLUKESKYWALKER02,
+  ],
+]);
+
+const uniqueAbilities = new Map([
+  [
+    "uniqueskill_COMMANDERLUKESKYWALKER01",
+    uniqueskill_COMMANDERLUKESKYWALKER01,
+  ],
+  [
+    "uniqueskill_COMMANDERLUKESKYWALKER02",
+    uniqueskill_COMMANDERLUKESKYWALKER02,
+  ],
+]);
+
+const leaderAbility = new Map([]);
+
+export default {
+  specialAbilities,
+  uniqueAbilities,
+  basicAbility,
+  leaderAbility,
+};
